04bc48f980b2bd8c427548b5fbb47fb62a8274b9
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-10-31 21:51:00 +0530

    sd

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,18 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [data, setData] = React.useState('')
+
+  React.useState(() => {
+    fetch('https://dummyjson.com/products')
+    .then(e => e.text())
+    .then(e => setData(e))
+  }, [])
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <pre>{data}</pre>
     </div>
   )
 }
